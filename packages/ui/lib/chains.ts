8b43580dbd53802234cc510e11dbc4c29c702cf4
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2026-01-09 11:05:10 -0300

    ui: limit l2 chain and tokens

--- packages/ui/lib/chains.ts
+++ packages/ui/lib/chains.ts
@@ -1,9 +1,7 @@
 const CHAINS_DICTIONARY: Record<string, { id: number; name: string }> = {
-  mainnet: { id: 1, name: 'Ethereum' },
   arbitrum: { id: 42161, name: 'Arbitrum' },
   base: { id: 8453, name: 'Base' },
   optimism: { id: 10, name: 'Optimism' },
-  gnosis: { id: 100, name: 'Gnosis' },
 }
 
 export type ChainKey = keyof typeof CHAINS_DICTIONARY
--- packages/ui/lib/tokens.ts
+++ packages/ui/lib/tokens.ts
@@ -15,20 +15,6 @@ export const TOKENS_DICTIONARY: Record<string, Record<string, { address: string;
     WBTC: { address: '0x0555e30da8f98308edb960aa94c0db47230d2b9c', decimals: 8 },
     WETH: { address: '0x4200000000000000000000000000000000000006', decimals: 18 },
   },
-  ethereum: {
-    USDC: { address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', decimals: 6 },
-    USDT: { address: '0xdac17f958d2ee523a2206206994597c13d831ec7', decimals: 6 },
-    DAI: { address: '0x6b175474e89094c44da98b954eedeac495271d0f', decimals: 18 },
-    WBTC: { address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', decimals: 8 },
-    WETH: { address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', decimals: 18 },
-  },
-  gnosis: {
-    USDC: { address: '0x2a22f9c3b484c3629090feed35f17ff8f88f76f0', decimals: 6 },
-    USDT: { address: '0x4ecaba5870353805a9f068101a40e0f32ed605c6', decimals: 6 },
-    WXDAI: { address: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d', decimals: 18 },
-    WBTC: { address: '0x8e5bbbb09ed1ebde8674cda39a0c169401db4252', decimals: 8 },
-    WETH: { address: '0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1', decimals: 18 },
-  },
   optimism: {
     USDC: { address: '0x0b2c639c533813f4aa9d7837caf62653d097ff85', decimals: 6 },
     USDT: { address: '0x94b008aa00579c1307b0ef2c499ad98a8ce58e58', decimals: 6 },
